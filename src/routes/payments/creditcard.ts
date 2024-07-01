3d66de07e597dc4cc342c42d82f1502672143066
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-07-01 10:12:08 -0300

    install mercado pago sdk and create route-get

--- src/routes/payments/creditcard.ts
+++ src/routes/payments/creditcard.ts
@@ -2,3 +2,6 @@ import {Router,Request,Response} from 'express'
 
 export const routeCreditCardPayment = Router()
 
+routeCreditCardPayment.get('/payments/creditcard/',(req:Request,res:Response)=>{
+
+})
--- src/routes/payments/pix.ts
+++ src/routes/payments/pix.ts
@@ -1,3 +1,4 @@
 import {Router,Request,Response} from 'express'
 
 export const routePixPayment = Router()
+
